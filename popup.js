3547fa2db2a9936ade07c2cc28a174ced9b9ab67
leepriscilla <lee.priscilla98@gmail.com>
2022-02-17 15:11:14 -0800

    added popup

---
+++ popup.js
@@ -0,0 +1,18 @@
+TODO: 
+
+// document.addEventListener('DOMContentLoaded', function(){
+//     var saveRecordingButton = document.getElementById('savedRecording');
+//     saveRecordingButton.addEventListener('start', function(){
+//         chrome.tabs.getSelected(null, function(tab){
+//             alert("Saved recordings!");
+//         });
+//     }, false);
+    
+//     var startRecordingButton = document.getElementByID('startRecording');
+//     startRecordingButton.addEventListener('start', function(){
+//         chrome.tabs.getSelected(null, function(tab){
+//             alert("Start recording!");
+//         });
+//     }, false);
+
+// }, false);
