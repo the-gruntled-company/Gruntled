e741dea42ccd926c403f69616b9b9063eaf9cd5b
leepriscilla <lee.priscilla98@gmail.com>
2022-02-20 13:38:38 -0800

    add tabs

--- popup.js
+++ popup.js
@@ -1,18 +1,25 @@
-TODO: 
-
-// document.addEventListener('DOMContentLoaded', function(){
-//     var saveRecordingButton = document.getElementById('savedRecording');
-//     saveRecordingButton.addEventListener('start', function(){
-//         chrome.tabs.getSelected(null, function(tab){
-//             alert("Saved recordings!");
-//         });
-//     }, false);
-    
-//     var startRecordingButton = document.getElementByID('startRecording');
-//     startRecordingButton.addEventListener('start', function(){
-//         chrome.tabs.getSelected(null, function(tab){
-//             alert("Start recording!");
-//         });
-//     }, false);
-
-// }, false);
+function setupTabs (){
+    document.querySelectorAll('.tab-btn').forEach(button=>{
+      button.addEventListener('click',()=>{
+        
+        
+        const sidebar = button.parentElement;
+        const tabs = sidebar.parentElement;
+        const tabNumber = button.dataset.forTab;
+        const tabActivate = tabs.querySelector(`.tab-content[data-tab="${tabNumber}"]`)
+        
+        sidebar.querySelectorAll('.tab-btn').forEach(button=>{
+          button.classList.remove('tab-btn-active')
+        })
+         tabs.querySelectorAll('.tab-content').forEach(tab=>{
+          tab.classList.remove('tab-content-active')
+        })
+        button.classList.add('tab-btn-active')
+        tabActivate.classList.add('tab-content-active')
+      })
+    })
+  }
+  
+  document.addEventListener('DOMContentLoaded',()=>{
+    setupTabs();
+  })
