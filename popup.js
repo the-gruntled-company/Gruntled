21ece4cc607a03e9d883d62cc031f14a6016f8d1
leepriscilla <lee.priscilla98@gmail.com>
2022-02-21 23:36:26 -0800

    add video

--- popup.js
+++ popup.js
@@ -2,7 +2,6 @@ function setupTabs (){
     document.querySelectorAll('.tab-btn').forEach(button=>{
       button.addEventListener('click',()=>{
         
-        
         const sidebar = button.parentElement;
         const tabs = sidebar.parentElement;
         const tabNumber = button.dataset.forTab;
@@ -18,8 +17,22 @@ function setupTabs (){
         tabActivate.classList.add('tab-content-active')
       })
     })
-  }
-  
-  document.addEventListener('DOMContentLoaded',()=>{
+}
+
+const captureVideoButton = document.querySelector(
+  "#cssfilters .capture-button"
+);
+const video = document.querySelector("#cssfilters video");
+captureVideoButton.onclick = function () {
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(handleSuccess)
+    .catch(handleError);
+};
+function handleSuccess(stream) {
+  video.srcObject = stream;
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
     setupTabs();
-  })
+})
