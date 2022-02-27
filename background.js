a388e28b1d8be32696ddaf4bea8fb0a00b62dcaf
leepriscilla <lee.priscilla98@gmail.com>
2022-02-27 02:31:18 -0800

    fixed webcam

--- background.js
+++ background.js
@@ -1,163 +1,25 @@
-/*Content script requests access to media devices
-On success, content script notifies background script
-On receipt of message, background script requests acccess to media devices; since content script has already succeeded, background script will now also succeed*/
-
 "use strict";
 
-function startRecord() {
-    chrome.windows.getCurrent({}, function(e) {
-        chrome.windows.create({
-            url: "camera.html",
-            type: "popup",
-            width: 266,
-            height: 200,
-        }, function(e) {
-            cameraWindowId = e.id, chrome.windows.onRemoved.addListener(function(e) {
-                e == cameraWindowId && console.log("window closed")
-            })
-        })
+function setupWebcam() {
+    navigator.mediaDevices.getUserMedia({
+        video: true
+    }).then(stream => {
+        chrome.storage.local.set({
+            'camAccess': true
+        }, () => {});
+        document.querySelector('#webcamVideo').srcObject = stream;
+        document.querySelector('#start').disabled = true;
+        document.querySelector('#record').disabled = false;
     })
+    .catch((error) => {
+        document.querySelector('#status').innerHTML = error.toString();
+        console.warn(error);
+    });
 }
 
-// 'use strict';
+document.addEventListener('DOMContentLoaded', ()=>{
+    setupWebcam();
+});
 
-// /* globals MediaRecorder */
-
-// let mediaRecorder;
-// let recordedBlobs;
-
-// const codecPreferences = document.querySelector('#codecPreferences');
-
-// const errorMsgElement = document.querySelector('span#errorMsg');
-// const recordedVideo = document.querySelector('video#recorded');
-// const recordButton = document.querySelector('button#record');
-// recordButton.addEventListener('click', () => {
-//   if (recordButton.textContent === 'Start Recording') {
-//     startRecording();
-//   } else {
-//     stopRecording();
-//     recordButton.textContent = 'Start Recording';
-//     playButton.disabled = false;
-//     downloadButton.disabled = false;
-//     codecPreferences.disabled = false;
-//   }
-// });
-
-// const playButton = document.querySelector('button#play');
-// playButton.addEventListener('click', () => {
-//   const mimeType = codecPreferences.options[codecPreferences.selectedIndex].value.split(';', 1)[0];
-//   const superBuffer = new Blob(recordedBlobs, {type: mimeType});
-//   recordedVideo.src = null;
-//   recordedVideo.srcObject = null;
-//   recordedVideo.src = window.URL.createObjectURL(superBuffer);
-//   recordedVideo.controls = true;
-//   recordedVideo.play();
-// });
-
-// const downloadButton = document.querySelector('button#download');
-// downloadButton.addEventListener('click', () => {
-//   const blob = new Blob(recordedBlobs, {type: 'video/webm'});
-//   const url = window.URL.createObjectURL(blob);
-//   const a = document.createElement('a');
-//   a.style.display = 'none';
-//   a.href = url;
-//   a.download = 'test.webm';
-//   document.body.appendChild(a);
-//   a.click();
-//   setTimeout(() => {
-//     document.body.removeChild(a);
-//     window.URL.revokeObjectURL(url);
-//   }, 100);
-// });
-
-// function handleDataAvailable(event) {
-//   console.log('handleDataAvailable', event);
-//   if (event.data && event.data.size > 0) {
-//     recordedBlobs.push(event.data);
-//   }
-// }
-
-// function getSupportedMimeTypes() {
-//   const possibleTypes = [
-//     'video/webm;codecs=vp9,opus',
-//     'video/webm;codecs=vp8,opus',
-//     'video/webm;codecs=h264,opus',
-//     'video/mp4;codecs=h264,aac',
-//   ];
-//   return possibleTypes.filter(mimeType => {
-//     return MediaRecorder.isTypeSupported(mimeType);
-//   });
-// }
-
-// function startRecording() {
-//   recordedBlobs = [];
-//   const mimeType = codecPreferences.options[codecPreferences.selectedIndex].value;
-//   const options = {mimeType};
-
-//   try {
-//     mediaRecorder = new MediaRecorder(window.stream, options);
-//   } catch (e) {
-//     console.error('Exception while creating MediaRecorder:', e);
-//     errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
-//     return;
-//   }
-
-//   console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
-//   recordButton.textContent = 'Stop Recording';
-//   playButton.disabled = true;
-//   downloadButton.disabled = true;
-//   codecPreferences.disabled = true;
-//   mediaRecorder.onstop = (event) => {
-//     console.log('Recorder stopped: ', event);
-//     console.log('Recorded Blobs: ', recordedBlobs);
-//   };
-//   mediaRecorder.ondataavailable = handleDataAvailable;
-//   mediaRecorder.start();
-//   console.log('MediaRecorder started', mediaRecorder);
-// }
-
-// function stopRecording() {
-//   mediaRecorder.stop();
-// }
-
-// function handleSuccess(stream) {
-//   recordButton.disabled = false;
-//   console.log('getUserMedia() got stream:', stream);
-//   window.stream = stream;
-
-//   const gumVideo = document.querySelector('video#gum');
-//   gumVideo.srcObject = stream;
-
-//   getSupportedMimeTypes().forEach(mimeType => {
-//     const option = document.createElement('option');
-//     option.value = mimeType;
-//     option.innerText = option.value;
-//     codecPreferences.appendChild(option);
-//   });
-//   codecPreferences.disabled = false;
-// }
-
-// async function init(constraints) {
-//   try {
-//     const stream = await navigator.mediaDevices.getUserMedia(constraints);
-//     handleSuccess(stream);
-//   } catch (e) {
-//     console.error('navigator.getUserMedia error:', e);
-//     errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
-//   }
-// }
-
-// document.querySelector('button#start').addEventListener('click', async () => {
-//   document.querySelector('button#start').disabled = true;
-//   const hasEchoCancellation = document.querySelector('#echoCancellation').checked;
-//   const constraints = {
-//     audio: {
-//       echoCancellation: {exact: hasEchoCancellation}
-//     },
-//     video: {
-//       width: 1280, height: 720
-//     }
-//   };
-//   console.log('Using media constraints:', constraints);
-//   await init(constraints);
-// });
+//Request webcam access from user upon installation
+//Implement stopWebcam()
--- popup.js
+++ popup.js
@@ -1,38 +1,105 @@
 'use strict';
 
-//On click: Start recording
-start.addEventListener('click', function(){
-  chrome.runtime.getBackgroundPage(function (backgroundPage) {
-		backgroundPage.startRecord();
-	})
-});
+let mediaRecorder;
+let recordedBlobs;
 
 function setupTabs (){
-    document.querySelectorAll('.tab-btn').forEach(button=>{
-      button.addEventListener('click', ()=>{
-        
-        const sidebar = button.parentElement;
-        const tabs = sidebar.parentElement;
-        const tabNumber = button.dataset.forTab;
-        const tabActivate = tabs.querySelector(`.tab-content[data-tab="${tabNumber}"]`)
-        
-        sidebar.querySelectorAll('.tab-btn').forEach(button=>{
-          button.classList.remove('tab-btn-active')
-        })
-         tabs.querySelectorAll('.tab-content').forEach(tab=>{
-          tab.classList.remove('tab-content-active')
-        })
-        button.classList.add('tab-btn-active')
-        tabActivate.classList.add('tab-content-active')
+  document.querySelectorAll('.tab-btn').forEach(button=>{
+    button.addEventListener('click', ()=>{
+      
+      const sidebar = button.parentElement;
+      const tabs = sidebar.parentElement;
+      const tabNumber = button.dataset.forTab;
+      const tabActivate = tabs.querySelector(`.tab-content[data-tab="${tabNumber}"]`)
+      
+      sidebar.querySelectorAll('.tab-btn').forEach(button=>{
+        button.classList.remove('tab-btn-active')
       })
+       tabs.querySelectorAll('.tab-content').forEach(tab=>{
+        tab.classList.remove('tab-content-active')
+      })
+      button.classList.add('tab-btn-active')
+      tabActivate.classList.add('tab-content-active')
     })
+  })
 }
 
-//On click: Close button
-var closeButton = document.querySelector('.closeButton');
+//id=close: close popup
+const closeButton = document.querySelector('#close');
 closeButton.addEventListener('click', function() {
   window.close();
 });
+
+//id=start: start webcam
+const startButton = document.querySelector('#start');
+startButton.addEventListener('click', function(){
+  chrome.tabs.create({
+    url: chrome.extension.getURL('welcome.html'),
+    active: true
+  });
+  chrome.runtime.getBackgroundPage(function (backgroundPage) {
+		backgroundPage.setupWebcam();
+	})
+});
+
+const recordButton = document.querySelector('#record');
+const playButton = document.querySelector('#play');
+const downloadButton = document.querySelector('#download');
+
+recordButton.addEventListener('click', ()=> {
+  if (recordButton.textContent === 'Start Recording') {
+    startRecording();
+  } else {
+    stopRecording();
+    recordButton.textContent = 'Start Recording';
+    playButton.disabled = false;
+    downloadButton.disabled = false;
+  }
+});
+
+function startRecording() {
+  //start
+  recordedBlobs = [];
+
+  try {
+    mediaRecorder = new MediaRecorder(window.stream);
+  } catch (e) {
+    console.error('Exception while creating MediaRecorder:', e);
+    errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
+    return;
+  }
+
+  console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
+  //end
+
+  recordButton.textContent = 'Stop Recording';
+  playButton.disabled = true;
+  downloadButton.disabled = true;
+  
+  //start
+  mediaRecorder.onstop = (event) => {
+    console.log('Recorder stopped: ', event);
+    console.log('Recorded Blobs: ', recordedBlobs);
+  };
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.start();
+  console.log('MediaRecorder started', mediaRecorder);
+  //end
+}
+
+function stopRecording() {
+  // mediaRecorder.stop();
+}
+//id=record: restart video and record
+//src=https://stackoverflow.com/questions/39098532/restart-html-video-with-javascript
+// var recordButton = document.querySelector('.closeButton');
+// closeButton.addEventListener('click', function() {
+//   var video = document.getElementById('vidId');
+//   video.pause();
+//   video.currentTime = 0;
+//   video.load();
+// });
+
 
 //On load
 document.addEventListener('DOMContentLoaded',()=>{
