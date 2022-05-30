4d82b3546dc5dd7ebfcfa2af2441c1035396160a
cvraut <chinmay0raut@gmail.com>
2022-05-30 11:59:55 -0700

    things changed & idk what exactly

--- vi_upload.py
+++ vi_upload.py
@@ -28,7 +28,7 @@ def main():
 
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = "client_secret_desktop.json"
+    client_secrets_file = "client_secret.json"
 
     # Get credentials and create an API client
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
