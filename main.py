22ec0daa78325b0e947860060938d8a5c40df5fb
cvraut <chinmay0raut@gmail.com>
2022-07-05 21:13:36 -0400

    ready for heroku migration

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from gruntled_backend import db, create_app, models
 
-# create actual backend object
 app = create_app()
-# create the db's
 db.create_all(app = app)
+
 if __name__ == '__main__':
+  # create actual backend object
+  app = create_app()
+  # create the db's
+  db.create_all(app = app)
   # runs the app
-  app.run(host="localhost",port=8080,debug = True)
+  app.run(host="localhost",port=5000,debug = True)
