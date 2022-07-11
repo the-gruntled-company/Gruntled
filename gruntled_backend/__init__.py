038d8dd6596fcc14db26febca785920797d03c61
Priscilla Kathryn lee <lee.priscilla98@gmail.com>
2022-07-11 08:54:23 -0700

    Initial migration

--- gruntled_backend/__init__.py
+++ gruntled_backend/__init__.py
@@ -3,9 +3,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager 
+from flask_migrate import Migrate
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -14,6 +16,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
