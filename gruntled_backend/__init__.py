6b01af4e35f44f7800ace06d3efcc9e507d6ffc2
Priscilla Kathryn lee <lee.priscilla98@gmail.com>
2022-07-11 09:43:30 -0700

    remove flask-migrate

--- gruntled_backend/__init__.py
+++ gruntled_backend/__init__.py
@@ -3,11 +3,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager 
-from flask_migrate import Migrate
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
-migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -16,7 +14,6 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ikxphdxdmysghq:8cef93eacb6830b8095c8e5d1b33ecacfaba8231ea400e0c62ca2ea1df5b1a7d@ec2-3-223-169-166.compute-1.amazonaws.com:5432/de9h18disn317r'
 
     db.init_app(app)
-    migrate.init_app(app, db)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
