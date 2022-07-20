ef7df81cc64164cbfd912e68e9a76819d146f11c
Priscilla Kathryn lee <lee.priscilla98@gmail.com>
2022-07-20 15:36:06 -0700

    get database uri via os.getenv

--- gruntled_backend/__init__.py
+++ gruntled_backend/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager 
 from flask_migrate import Migrate
-import subprocess
+import os
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
@@ -14,15 +14,12 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
-        
-    #Get Database URI
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ikxphdxdmysghq:8cef93eacb6830b8095c8e5d1b33ecacfaba8231ea400e0c62ca2ea1df5b1a7d@ec2-3-223-169-166.compute-1.amazonaws.com:5432/de9h18disn317r'
 
-    heroku_app_name = "gruntled-api-dev"
-    url = subprocess.run(["heroku", "config:get", "DATABASE_URL", "--app", heroku_app_name], capture_output=True).stdout 
-    url = str(url).replace("b'postgres", "postgresql", 1)
-    url = str(url).replace("\\n'", "", 1)
-    app.config['SQLALCHEMY_DATABASE_URI'] = url
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ikxphdxdmysghq:8cef93eacb6830b8095c8e5d1b33ecacfaba8231ea400e0c62ca2ea1df5b1a7d@ec2-3-223-169-166.compute-1.amazonaws.com:5432/de9h18disn317r'
+
+    uri = os.getenv("DATABASE_URL")
+    uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
     db.init_app(app)
     migrate.init_app(app, db)
