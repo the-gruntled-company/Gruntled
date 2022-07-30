7e1366b799c62190c92a0fb3efb59404ddc24e1d
cvraut <chinmay0raut@gmail.com>
2022-07-29 20:04:25 -0400

    got the 🏪 up!

--- credential_store.py
+++ credential_store.py
@@ -26,11 +26,11 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 
 # Setup the Drive v3 API
-SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
+SCOPES = 'https://www.googleapis.com/auth/drive'
 store = file.Storage('credentials.json')
 creds = store.get()
 if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+    flow = client.flow_from_clientsecrets('client_secrets.json', SCOPES)
     creds = tools.run_flow(flow, store)
 service = build('drive', 'v3', http=creds.authorize(Http()))
 
@@ -45,3 +45,5 @@ else:
     for item in items:
         print('{0} ({1})'.format(item['name'], item['id']))
 # [END drive_quickstart]
+
+
--- drive_store_upload.py
+++ drive_store_upload.py
@@ -1,15 +1,67 @@
 # drive_store_upload.py
 # this file is an example to upload crap to the gdrive account
+# follows this tut: https://www.thepythoncode.com/article/using-google-drive--api-in-python
 
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+# from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.http import MediaFileUpload
 
-gauth = GoogleAuth()           
-drive = GoogleDrive(gauth)
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly',
+          'https://www.googleapis.com/auth/drive.file']
 
-upload_file_list = ['drive_store_upload.py']
-for upload_file in upload_file_list:
-	gfile = drive.CreateFile({'parents': [{'id': '1QiVjETYMJNp3i-yENfwR_rUwK_GQdGHi'}]})
-	# Read file and set it as the content of this instance.
-	gfile.SetContentFile(upload_file)
-	gfile.Upload() # Upload the file.
+SCOPES = 'https://www.googleapis.com/auth/drive'
+
+def get_gdrive_service():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('client_secrets.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    # return Google Drive API service
+    return build('drive', 'v3', credentials=creds)
+
+def upload_files(fname = "test.txt",upload_folder="1QiVjETYMJNp3i-yENfwR_rUwK_GQdGHi"):
+    """
+    Creates a folder and upload a file to it
+    """
+    # authenticate account
+    service = get_gdrive_service()
+    # folder details we want to make
+    folder_metadata = {
+        "name": "TestFolder",
+        "mimeType": "application/vnd.google-apps.folder",
+        "parents": [upload_folder]
+    }
+    # create the folder
+    file = service.files().create(body=folder_metadata, fields="id").execute()
+    # get the folder id
+    folder_id = file.get("id")
+    print("Folder ID:", folder_id)
+    # upload a file text file
+    # first, define file metadata, such as the name and the parent folder ID
+    file_metadata = {
+        "name": fname,
+        "parents": [folder_id]
+    }
+    # upload
+    media = MediaFileUpload(fname, resumable=True)
+    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    print("File created, id:", file.get("id"))
+
+upload_files(fname = "drive_store_upload.py")
