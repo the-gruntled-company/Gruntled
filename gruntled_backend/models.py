d589adaed1e48038db294f094b4892d90d1bd31a
Priscilla Kathryn lee <lee.priscilla98@gmail.com>
2022-08-22 16:25:23 -0700

    rename username to name

--- gruntled_backend/models.py
+++ gruntled_backend/models.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 class User(UserMixin, db.Model):
     id= db.Column(db.Integer, primary_key=True) #primary keys are required by SQLAlchemy
-    username= db.Column(db.String(100), unique=True, nullable=False)
+    name= db.Column(db.String(100), unique=True, nullable=False)
     email= db.Column(db.String(100), unique=True, nullable=False)
     password= db.Column(db.String(100), nullable=False)
     date_create= db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
