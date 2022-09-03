cda96c6949cad22653e9672c1a580c34b5821fc5
Priscilla Kathryn lee <lee.priscilla98@gmail.com>
2022-09-02 19:19:42 -0700

    added models properly phew

--- gruntled_backend/models.py
+++ gruntled_backend/models.py
@@ -1,33 +1,31 @@
 # models.py
 
 from flask_login import UserMixin
-from . import db
 from datetime import datetime
+from . import db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    name = db.Column(db.String(1000), nullable=False)
+    name = db.Column(db.String(1000), nullable=False, unique=True,)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     date_create= db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    date_access= db.Column(db.DateTime, nullable=False)
+    date_access= db.Column(db.DateTime)
 
 class Youtube(db.Model):
     id= db.Column(db.Integer, primary_key=True) #primary keys are required by SQLAlchemy
     title= db.Column(db.String(100), nullable=False)
     link= db.Column(db.String(100), unique=True, nullable=False)
-    # user_id= db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) #user that watches YT
     description= db.Column(db.Text)
     like_count= db.Column(db.Integer, nullable=False)
     view_count= db.Column(db.Integer, nullable=False)
-    date_last_updated= db.Column(db.DateTime, nullable=False)
-    # date_access= db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_updated= db.Column(db.DateTime, nullable=False)
 
 class Reaction(db.Model):
     id= db.Column(db.Integer, primary_key=True) #primary keys are required by SQLAlchemy
     title= db.Column(db.String(100), nullable=False)
     link= db.Column(db.String(100), unique=True, nullable=False)
-    user_id= db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) #user that uploads reaction
+    uploader= db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) #user that uploads reaction
     public= db.Column(db.Boolean(), nullable=False, default=False)
     view_count= db.Column(db.Integer, nullable=False, default=0)
     date_create= db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
@@ -38,7 +36,7 @@ class Merge(db.Model):
     id= db.Column(db.Integer, primary_key=True) #primary keys are required by SQLAlchemy
     title= db.Column(db.String(100), nullable=False)
     link= db.Column(db.String(100), unique=True, nullable=False)
-    user_id= db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) #user that uploads reaction
+    uploader= db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) #user that uploads reaction
     public= db.Column(db.Boolean(), nullable=False, default=False)
     view_count= db.Column(db.Integer, nullable=False, default=0)
     date_create= db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
